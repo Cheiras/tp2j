14d0bb142d5a8fc25176d7b8b996027dc2d6e408
Cheiras <dtcheche@gmail.com>
2017-11-19 11:21:09 -0300

    Test reduce dinero del jugador al comprar propiedad.

---
+++ test/testIntegracionSegundaEntrega/ComprarUnBarrioReduceElDineroDelJugadorTest.java
@@ -0,0 +1,23 @@
+package testIntegracionSegundaEntrega;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import caible.propiedades.barrios.Barrio;
+import caible.propiedades.barrios.BarrioNormal;
+import partida.Jugador;
+
+public class ComprarUnBarrioReduceElDineroDelJugadorTest {
+
+	Jugador unJugador = new Jugador(100000, null);
+	Barrio BuenosAiresSur = new BarrioNormal("Buenos Aires Sur", 20000, unJugador, 2, 2000, 3000, 3500, 5000, 5000,
+			8000);
+
+	@Test
+	public void test() {
+		BuenosAiresSur.accionar(unJugador);
+
+		Assert.assertEquals(80000, unJugador.getEfectivo(), 0.1);
+	}
+
+}
