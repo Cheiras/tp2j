b3d781a1ee5b9f557181a0928880f84d2ebf97de
Cheiras <dtcheche@gmail.com>
2017-11-20 16:01:29 -0300

    Cambios en Partida.

--- src/partida/Partida.java
+++ src/partida/Partida.java
@@ -8,11 +8,11 @@ import partida.tablero.Tablero;
 
 public class Partida {
 
-	//bla
 	private Jugador jugadorActual;
-	private Jugador jugador1 = new Jugador("J1", 100000, null);
-	private Jugador jugador2 = new Jugador("J2", 100000, null);
-	private Jugador jugador3 = new Jugador("J3", 100000, null);
+	private Jugador jugador1;
+	private Jugador jugador2;
+	private Jugador jugador3;
+	private Tablero tablero;
 	private ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
 	private int turno = 0;
 
@@ -20,14 +20,16 @@ public class Partida {
 
 		Dado dado1 = new Dado(6);
 		Dado dado2 = new Dado(6);
-
+		jugador1  = new Jugador("J1", 100000, null); 
+		jugador2  = new Jugador("J1", 100000, null); 
+		jugador3  = new Jugador("J1", 100000, null); 
 		jugadores.add(jugador1);
 		jugadores.add(jugador2);
 		jugadores.add(jugador3);
 		Collections.shuffle(jugadores);
 		jugadorActual = jugadores.get(turno % 3);
 
-		Tablero tablero = new Tablero();
+		tablero = new Tablero();
 
 	}
 
