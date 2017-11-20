22baa4f961238d547a3fc0f9d25f5d24398abf96
Cheiras <dtcheche@gmail.com>
2017-11-20 13:13:41 -0300

    Agrego lista de jugadores aleatoria.

--- src/partida/Partida.java
+++ src/partida/Partida.java
@@ -8,34 +8,30 @@ import partida.tablero.Tablero;
 
 public class Partida {
 
-	Jugador jugadorActual;
+	private Jugador jugadorActual;
+	private Jugador jugador1 = new Jugador("J1", 100000, null);
+	private Jugador jugador2 = new Jugador("J2", 100000, null);
+	private Jugador jugador3 = new Jugador("J3", 100000, null);
+	private ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
+	private int turno = 0;
 
 	public Partida() {
-		
+
 		Dado dado1 = new Dado(6);
 		Dado dado2 = new Dado(6);
 
-		
-		Jugador jugador1 = new Jugador("J1", 100000, null);
-		Jugador jugador2 = new Jugador("J2", 100000, null);
-		Jugador jugador3 = new Jugador("J3", 100000, null);
-		ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
-		System.out.println(jugador1);
 		jugadores.add(jugador1);
 		jugadores.add(jugador2);
 		jugadores.add(jugador3);
-		
 		Collections.shuffle(jugadores);
-		
+		jugadorActual = jugadores.get(turno % 3);
 
 		Tablero tablero = new Tablero();
-		
-		
-		
 
 	}
 
 	public Jugador jugadorActual() {
 		return jugadorActual;
 	}
+
 }
