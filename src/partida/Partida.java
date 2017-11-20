583ff9f51bfde2c007a55321ef6b021a955f90f6
Cheiras <dtcheche@gmail.com>
2017-11-20 17:47:41 -0300

    Arreglos.

--- src/partida/Partida.java
+++ src/partida/Partida.java
@@ -3,12 +3,14 @@ package partida;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import javafx.application.Application;
 import movimiento.Dado;
 import movimiento.Tirador;
 import partida.jugador.Jugador;
 import partida.tablero.Tablero;
+import partida.turno.Turno;
 
-public class Partida {
+public class Partida{
 
 	private Jugador jugadorActual;
 	private Jugador jugador1;
@@ -17,34 +19,32 @@ public class Partida {
 	private Tablero tablero;
 	private Tirador tirador;
 	private ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
-	private int turno = 0;
+	private int turnos = 0;
 
 	public Partida() {
 
 		Dado dado1 = new Dado(6);
 		Dado dado2 = new Dado(6);
-		tirador = new Tirador(dado1,dado2);
-		jugador1  = new Jugador("J1", 100000, null); 
-		jugador2  = new Jugador("J1", 100000, null); 
-		jugador3  = new Jugador("J1", 100000, null); 
+		tirador = new Tirador(dado1, dado2);
+		jugador1 = new Jugador("J1", 100000, null);
+		jugador2 = new Jugador("J1", 100000, null);
+		jugador3 = new Jugador("J1", 100000, null);
 		jugadores.add(jugador1);
 		jugadores.add(jugador2);
 		jugadores.add(jugador3);
 		Collections.shuffle(jugadores);
-		jugadorActual = jugadores.get(turno % 3);
+		jugadorActual = jugadores.get(turnos % 3);
 
 		tablero = new Tablero();
-		
-		
 
 	}
 
 	public Jugador jugadorActual() {
 		return jugadorActual;
 	}
-	
-	public int getTurno(){
-		return this.turno;
+
+	public int getTurno() {
+		return this.turnos;
 	}
 
 }
