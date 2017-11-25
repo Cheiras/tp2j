f627ebd6f608dc2741f1a9edd2e8e7ce34757c5b
Luciano <lucianortiz97@gmail.com>
2017-11-25 14:35:12 -0300

    Arreglados test Tirador

--- test/testMovimiento/TiradorTest.java
+++ test/testMovimiento/TiradorTest.java
@@ -3,6 +3,7 @@ package testMovimiento;
 import org.junit.Assert;
 import org.junit.Test;
 
+import excepciones.NumeroDobleEnDadosException;
 import movimiento.Dado;
 import movimiento.Tirador;
 
@@ -12,20 +13,29 @@ public class TiradorTest {
 	Dado dado2 = new Dado(6);
 	Tirador tirador = new Tirador(dado1, dado2);
 
-	@Test
+	@Test 
 	public void lanzaValoresCorrectosTest() {
 		for (int i = 0; i < 100; i++) {
-			int valor = tirador.tirar();
-			Assert.assertTrue(1 <= valor && valor <= 12);
+			try {
+				int valor = tirador.tirar();
+				Assert.assertTrue(1 <= valor && valor <= 12);
+			}
+			catch(NumeroDobleEnDadosException excepcion) {
+				//Dados dobles, no hace nada.
+			}
 		}
 	}
 
 	@Test
 	public void DadosSonIgualesTest() {
 		for (int i = 0; i < 100; i++) {
-			tirador.tirar();
-			Assert.assertEquals(tirador.getValorDado1() == tirador.getValorDado2(), tirador.dadosMismoValor());
-
+			try {
+				tirador.tirar();
+				Assert.assertEquals(tirador.getValorDado1() == tirador.getValorDado2(), tirador.dadosMismoValor());
+			}
+			catch(NumeroDobleEnDadosException excepcion) {
+				//Dados dobles, no hace nada.
+			}
 		}
 	}
 }
