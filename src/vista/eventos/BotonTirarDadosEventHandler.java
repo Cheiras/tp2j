dac51ce57022d542149fb159b4b7d9f43b94c60e
Andivisciglio <atvisciglio@gmail.com>
2017-11-27 12:14:35 -0300

    Agrego el tablero a la pagina principal
    Queda detallar el borde inferior para que no se vea repetida la imagen

--- src/vista/eventos/BotonTirarDadosEventHandler.java
+++ src/vista/eventos/BotonTirarDadosEventHandler.java
@@ -25,6 +25,7 @@ public class BotonTirarDadosEventHandler implements EventHandler<ActionEvent> {
         }catch(RuntimeException e) {
         	this.handle(actionEvent);
         }
+        
         ///stage.setScene(proximaEscena);
         
     }
