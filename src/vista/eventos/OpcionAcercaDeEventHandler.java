eb18262eb46abe1c756a6752e2841da7f2c4aa63
Cheiras <dtcheche@gmail.com>
2017-11-26 21:20:39 -0300

    Instrucciones.

--- src/vista/eventos/OpcionAcercaDeEventHandler.java
+++ src/vista/eventos/OpcionAcercaDeEventHandler.java
@@ -7,13 +7,15 @@ import javafx.scene.control.Alert.AlertType;
 
 public class OpcionAcercaDeEventHandler implements EventHandler<ActionEvent> {
 
-    @Override
-    public void handle(ActionEvent actionEvent) {
-        Alert alert = new Alert(AlertType.INFORMATION);
-        alert.setTitle("Acerca de...");
-        alert.setHeaderText("Instrucciones");
-        String mensaje = "75.07 Algoritmos y  programaciÃ³n III.";
-        alert.setContentText(mensaje);
-        alert.show();
-    }
+	@Override
+	public void handle(ActionEvent actionEvent) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Instrucciones.");
+		alert.setHeaderText("Instrucciones.");
+		String mensaje = "1) A través de vender, comprar y alquilar propiedades, convertirse en el jugador más rico y, a la vez, conducir al resto a la bancarrota."
+				+ "\n2) Antes de comenzar el juego, todos los participantes recibirán del 100.000 pesos."
+				+ "\nEl primer jugador lanza los dados y mueve su ficha alrededor del tablero, en la dirección de la flecha, el número de espacios indicado por el total de ambos dados.  Los turnos corren en el sentido de las agujas del reloj alrededor de la mesa; cada jugador arroja los dados y avanza su ficha el número indicado de casillas, aprovechando las opciones que le da el resultado de su tirada. La ficha se quedará en esa casilla hasta que se produzca una nueva tirada.  Una o varias fichas pueden permanecer simultáneamente en la misma casilla.  Si un jugador obtiene un doble, tiene el derecho a tirar de nuevo.";
+		alert.setContentText(mensaje);
+		alert.show();
+	}
 }
