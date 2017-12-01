fd2f3f07991499935ccd70aa397832017a0febd2
Luciano <lucianortiz97@gmail.com>
2017-12-01 20:31:34 -0300

    Arregle el PoliciaTest

--- test/testIntegracionPrimerEntrega/PoliciaTest.java
+++ test/testIntegracionPrimerEntrega/PoliciaTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import caible.especiales.Carcel;
 import caible.especiales.Policia;
 import casilleros.Caible;
+import movimiento.MovimientoNormal;
 import partida.jugador.Jugador;
 
 
@@ -14,9 +15,10 @@ public class PoliciaTest {
 
 	@Test
 	public void test() {
+		MovimientoNormal mov = new MovimientoNormal();
 		Caible carcel = new Carcel();
 		Caible policia = new Policia(carcel);
-		Jugador jugador = new Jugador("", 0, null);
+		Jugador jugador = new Jugador("", 0, mov);
 		policia.accionar(jugador);
 		
 		Assert.assertEquals(jugador.getIndice(), carcel.getPosicion());
